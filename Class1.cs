9444ac89110d938a8031947d4a381d311957ee35
Julia72573 <juliakosniowska@gmail.com>
2026-02-16 23:09:17 +0100

    Dodaj pliki projektów.

---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VetCare_Manager
+{
+    internal class Class1
+    {
+    }
+}
---
+++ Pet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VetCare_Manager
+{
+	public class Pet
+	{
+		public string? Imie { get; set; }
+		public string? Rasa { get; set; }
+		public string? Gatunek { get; set; }
+		public DateTime DataUrodzenia { get; set; }
+		public int OwnerId { get; set; }
+	}
+}
